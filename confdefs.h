b0dd76b357367fa8647da837a45cbcf0f7f7c642
Raul Fernandez Ortega <natambio.audio@gmail.com>
2026-06-22 22:05:03 +0200

    Más cambios

---
+++ confdefs.h
@@ -0,0 +1,7 @@
+/* confdefs.h */
+#define PACKAGE_NAME "NatAmbio"
+#define PACKAGE_TARNAME "NatAmbio"
+#define PACKAGE_VERSION "1.0.0"
+#define PACKAGE_STRING "NatAmbio 1.0.0"
+#define PACKAGE_BUGREPORT "natambio.audio@gmail.com"
+#define PACKAGE_URL ""
